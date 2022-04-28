ce8b874556ff292d520a557df8f37c32feaac5e7
Abdullah <abdullah.ayoub2000@gmail.com>
2022-04-28 02:46:48 +0200

    adding template css, js files

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,13 +18,40 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
+    script: [
+      { src: '/js/core/popper.min.js',type: "text/javascript" },
+      { src: '/js/core/bootstrap.min.js',type: "text/javascript" },
+      { src: '/js/material-kit.min.js',type: "text/javascript" },
+      { src: '/js/material-kit.js.map',type: "application/json" },
+      { src: '/js/plugins/perfect-scrollbar.min.js',type: "text/javascript" },
+      { src: '/js/plugins/countup.min.js',type: "text/javascript" },
+      { src: '/js/plugins/choices.min.js',type: "text/javascript" },
+      { src: '/js/plugins/prism.min.js',type: "text/javascript" },
+      { src: '/js/plugins/highlight.min.js',type: "text/javascript" },
+      { src: '/js/plugins/rellax.min.js',type: "text/javascript" },
+      { src: '/js/plugins/tilt.min.js',type: "text/javascript" },
+      { src: 'js/plugins/parallax.min.js',type: "text/javascript" },
+      { src: 'js/plugins/flatpickr.min.js',type: "text/javascript" },
+      { src: 'js/plugins/moment.min.js',type: "text/javascript" },
+      { src: 'js/plugins/typedjs.js',type: "text/javascript" },
+
+
+    ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' },
+      { rel: 'stylesheet', href: '/css/material-icons.css'},
+      { rel: 'stylesheet', href: '/css/nucleo-icons.css' },
+      { rel: 'stylesheet', href: '/css/nucleo-svg.css' },
+      { rel: 'stylesheet', href: '/css/material-kit.min.css' },
+      { rel: 'application/json', href: '/css/material-kit.css.map' },
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@mdi/font/css/materialdesignicons.min.css',
+
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
