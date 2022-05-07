48d20d48ec17252f468529e152173fee22b9b1a3
abdullah3553 <abdullah.ayoub2000@gmail.com>
2022-05-07 08:17:04 +0200

    navigation bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,7 @@
       app
       color="dark"
       flat
+      clipped-left
     >
       <v-container>
         <v-row align="center" align-content="center" justify="center">
@@ -33,24 +34,47 @@
 
       </v-container>
     </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      mini-variant
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
 
     <v-main class="dark darken-3">
-      <v-btn
-        v-show="fab"
-        v-scroll="fabCounter"
-        dark
-        fixed
-        bottom
-        right
-        color="error"
-        @click="goToTop"
-      >
-        <v-icon>mdi-arrow-up</v-icon>
-      </v-btn>
+
       <Nuxt/>
 
     </v-main>
+    <v-btn
+      v-show="fab"
+      v-scroll="fabCounter"
+      dark
+      fixed
+      bottom
+      right
+      color="error"
+      @click="goToTop"
+    >
+      <v-icon>mdi-arrow-up</v-icon>
+    </v-btn>
   </v-app>
 
 </template>
@@ -70,7 +94,14 @@ export default {
         'Github',
         'About',
       ],
-      fab:false
+      fab:false,
+      drawer: true,
+      items: [
+        { title: 'Home', icon: 'mdi-home-city' },
+        { title: 'My Account', icon: 'mdi-account' },
+        { title: 'Users', icon: 'mdi-account-group-outline' },
+      ],
+      mini: true,
     }
   },
   methods:{
