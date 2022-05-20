d61395a37d7861e152fb810b74fe9f765c7bb3ce
abdullah3553 <abdullah.ayoub2000@gmail.com>
2022-05-21 01:07:35 +0200

    all pages added and added to router

--- router.js
+++ router.js
@@ -6,8 +6,13 @@ const page = path => () => import(`~/pages/${path}`).then(m => m.default || m)
 
 const routes = [
   // user routing
-  {path: '/', name: 'home', component: page('index.vue')},
+  {path: '/', name: 'home', component: page('home.vue')},
   {path: '/login', name: 'login', component: page('login.vue')},
+  {path: '/register', name: 'register', component: page('register.vue')},
+  {path: '/about', name: 'about', component: page('about.vue')},
+
+  {path: '/test', name: 'test', component: page('test.vue')},
+
 
 ];
 
